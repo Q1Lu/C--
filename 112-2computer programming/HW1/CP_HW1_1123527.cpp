5f8a70be1b4b39dad3a73d1d16d7e90fb1a209b4
Q1Lu <arthurx72189@gmail.com>
2024-03-08 02:53:20 +0800

    init

--- 112-2computer programming/HW1/CP_HW1_1123527.cpp
+++ 112-2computer programming/HW1/CP_HW1_1123527.cpp
@@ -1,23 +1,48 @@
 #include <iostream>
 using namespace std;
 
+class Dessert{
+    protected:
+        int weight = 0;
+    public:
+        Dessert(){
+            weight = 100;
+        };
+
+        int getWeight()const{
+            return weight;
+        };
+        virtual void eat(){
+            weight = weight -20;
+//          weight -= 20  
+        };
+};
+class Suncake : public Dessert {
+    public:
+        void eat() override {
+           cout << "Adding milk..." << endl;
+           Dessert::weight -= 20;
+        }
+};
+
+
 int main()
 {
  Dessert *d;
  int choice;
  while ( cin >> choice )
  {
- switch ( choice )
- {
- case 1:
- d = new Dessert();
- break;
- case 2:
- d = new Suncake();
- break;
- default:
- exit( 1 );
- }
+    switch ( choice )
+    {
+        case 1:
+            d = new Dessert();
+            break;
+        case 2:
+            d = new Suncake();
+            break;
+        default:
+            exit( 1 );
+    }
 
  cout << "The initial weight is " << d->getWeight() << '\n';
  d->eat();
