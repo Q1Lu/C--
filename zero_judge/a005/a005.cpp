154e23f79ebd240796f4e9b0b2d9bc1fa7c93e17
Q1Lu <arthurx72189@gmail.com>
2024-09-30 01:38:05 +0800

    2024/9/30a005 unfinished

---
+++ zero_judge/a005/a005.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int times = 0;
+    cin >> times;
+    for(int i = 0; i < times; i++){
+        int input[5] = {0};
+        for(int j = 0; j < 4; j++){
+            
+        }
+    }
+}
